8975f5ada72338f3e5cb270db6c97a3feb755544
ilean05 <45613180+ilean05@users.noreply.github.com>
2019-06-18 13:59:21 -0500

    asdf

---
+++ WindowsFormsApp2/WindowsFormsApp2/Form2.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form2 : Form
+    {
+
+        public Form2(string name, string categoria, string plantel, string cel, string mail, string adress)
+        {
+            InitializeComponent();
+            label7.Text = name;
+            label8.Text = categoria;
+            label9.Text = plantel;
+            textBox2.Text = cel;
+            textBox3.Text = mail;
+            textBox4.Text = adress;
+
+            
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        { 
+            
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+}
